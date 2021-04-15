ccb1e090518cbae2ab48cc59c4595b374cfc9df3
alantollett <alantollett@outlook.com>
2021-04-15 11:13:28 +0100

    changed database name

--- database.js
+++ database.js
@@ -9,7 +9,7 @@ function getDatabaseConnection() {
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
-        database: "portfolio"
+        database: "portfolio_optimiser"
     });
 
     return {
