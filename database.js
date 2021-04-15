571a4882fe3055118a87ee2232836d27e9230527
alantollett <alantollett@outlook.com>
2021-04-15 11:14:36 +0100

    database name now loaded from .env

--- database.js
+++ database.js
@@ -9,7 +9,7 @@ function getDatabaseConnection() {
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
-        database: "portfolio_optimiser"
+        database: process.env.MYSQL_DATABASE
     });
 
     return {
